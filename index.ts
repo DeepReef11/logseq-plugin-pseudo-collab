e0e7d1f9bd630ff256572bafd848b89a7a711588
jo <jonathan_92222@hotmail.com>
2023-05-19 16:49:16 -0400

    workingm, added inactive timeout

--- index.ts
+++ index.ts
@@ -16,6 +16,14 @@ const settings: SettingSchemaDesc[] = [
         description: 'Select a unique username for this device.',
         default: Date.now(),
     },
+
+    {
+        key: 'inactive',
+        type: 'number',
+        title: 'Inactive',
+        description: 'How long (in minutes) a user have to be inactive to stop notifying the user is on the same page. Set to 0 to always notify.',
+        default: 5,
+    },
 ]
 
 async function getAllPCPages(): Promise<any[]> {
@@ -58,12 +66,7 @@ async function getUserPCPage(currentUser: string): Promise<any | null> {
 
 async function main() {
     logseq.App.showMsg('Pseudo collab plugin enabled');
-    //let userInfo = await logseq.App.getUserInfo();
-
-    //console.log("Run: " + new Date());
-
     await sleep(1000);
-
     //   let graph = await logseq.app.getcurrentgraph();
     //   let folder = graph.path;
     //   let data = "data";
@@ -71,35 +74,34 @@ async function main() {
     //    fs.writefilesync(join(folder, filename), data, {
     //   flag: 'w',
     // });
-
     // const contents = fs.readfilesync(join(folder, filename), 'utf-8');
     // console.log(contents); // 👉️ "one two three four"
 
-
     let PCPages = await getAllPCPages();
 
-    let currentUser = "test";
+    let currentUser = logseq.settings?.currentUser;
     let userCollabPageName = pagePrefix + currentUser;
     let userCollabPage = await getUserPCPage(currentUser);
-    // PCPages.forEach(p => {
 
-    //     if (p.originalName == userCollabPageName) {
-    //         console.log("found");
-    //         userCollabPage = p
-    //     }
-    // })
     if (!userCollabPage) {
         console.log("not found");
 
         userCollabPage = await logseq.Editor.createPage(userCollabPageName, {}, { redirect: false, createFirstBlock: true, format: "markdown", journal: false });
 
     }
-    console.log(userCollabPage);
+
     let blocks = await logseq.App.getPageBlocksTree(userCollabPage.uuid);
     let targetBlock = blocks[0];
     console.log(targetBlock);
 
     await logseq.Editor.updateBlock(targetBlock.uuid, blockPrefix);
+
+    logseq.onSettingsChanged(async (settings) => {
+        logseq.Editor.deletePage(pagePrefix + currentUser);
+        currentUser = settings?.currentUser;
+        userCollabPageName = pagePrefix + currentUser;
+        userCollabPage = await logseq.Editor.createPage(userCollabPageName, {}, { redirect: false, createFirstBlock: true, format: "markdown", journal: false });
+    });
 
     //console.log(userInfo);
     //logseq.App.showMsg(userInfo);
@@ -137,15 +139,28 @@ async function main() {
                 if (content) {
 
                     let arr = content.split('\n');
-                    let i = arr.findIndex(l => {
-                        return l.startsWith('originalName');
-                    });
-                    let oName = arr[i].split(':')[1];
-                    if (oName === originalName) {
+                    let iLastActivity = arr.findIndex(l => { return l.startsWith('date:') });
+                    if (iLastActivity >= 0) {
+                        let lastActivity = arr[iLastActivity].substring(5); // 5 is length of "date:"
+                        console.log(lastActivity + ", " + arr[iLastActivity]);
+                        console.log(+lastActivity + " +time: " +  (logseq.settings?.inactive * 60000));
+                        if (logseq.settings?.inactive > 0 && Date.now() < +lastActivity + (logseq.settings?.inactive * 60000)) {
 
-                        return {
-                            username: p.originalName.substring(pagePrefix.length),
-                            pageOriginalName: oName
+
+                            let iOriginalName = arr.findIndex(l => {
+                                return l.startsWith('originalName');
+                            });
+                            if (iOriginalName >= 0) {
+                                let oName = arr[iOriginalName].split(':')[1];
+
+                                if (oName === originalName) {
+
+                                    return {
+                                        username: p.originalName.substring(pagePrefix.length),
+                                        pageOriginalName: oName
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -167,7 +182,7 @@ async function main() {
             let targetBlock = blocks[0];
             console.log(targetBlock);
             let isEditing = page.originalName
-            let editing = blockPrefix + "date:" + Date.now + "\nuuid:" + page.uuid + "\noriginalName:" + isEditing;
+            let editing = blockPrefix + "date:" + Date.now() + "\nuuid:" + page.uuid + "\noriginalName:" + isEditing;
             await logseq.Editor.updateBlock(targetBlock.uuid, editing);
             let otherIsEditing = await isPageCurrentlyEditing(isEditing);
             if (otherIsEditing) {
@@ -216,5 +231,5 @@ async function main() {
 
 
 // bootstrap
-logseq.ready(main).catch(console.error)
+logseq.useSettingsSchema(settings).ready(main).catch(console.error)
 
